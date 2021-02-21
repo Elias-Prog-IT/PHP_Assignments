79e2dba722c7cfc82bdbf5c44a13e0ec9f808f00
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-02-21 07:14:15 +0100

    final

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!--
+@author: Elias Posluk
+Student-id: elipos-5
+Uppgift: footer.php
+@date 20/02/2021
+Luleå tekniska universitet
+Webbutveckling II - Skriptspråk och databaser
+-->
+<p class="footer">
+    Luleå tekniska universitet | Webbutveckling II - Skriptspråk och databaser | elipos-5 | 2021
+</p>
