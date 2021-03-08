6dd84a4cc6bc6e253b6d1b5f8d2319acf68c01e2
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-03-08 10:11:56 +0100

    final

---
+++ Luleå University of Technology/Assignment_2/logout.php
@@ -0,0 +1,18 @@
+<!--
+@author: Elias Posluk
+Student-id: elipos-5
+Uppgift 2, logga ut sida
+@date 06/03/2021
+Luleå tekniska universitet
+Webbutveckling II - Skriptspråk och databaser
+-->
+
+<?php
+//Loggar ut användaren och omdirigerar tillbaka till login sidan. login.php
+session_start();
+
+$_SESSION['user_loggedin'] = "";
+
+header('location:login.php');
+
+?>
