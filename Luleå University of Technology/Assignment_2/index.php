310088aae96ffe1f71b0200205f70c3b4ca5d5cb
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-04-26 19:18:03 +0200

    final

--- Luleå University of Technology/Assignment_2/index.php
+++ Luleå University of Technology/Assignment_2/index.php
@@ -2,7 +2,7 @@
 @author: Elias Posluk
 Student-id: elipos-5
 Uppgift 2, innanför loginsidan. index.php
-@date 06/03/2021
+@date 26/04/2021
 Luleå tekniska universitet
 Webbutveckling II - Skriptspråk och databaser
 -->
@@ -30,7 +30,7 @@ if(isset($_SESSION['user_loggedin']) && $_SESSION['user_loggedin'] == 1){ ?>
         <tr>
             <td>
                 <form action="logout.php" method="get" style="display: inline">	<!-- knappen loggar ut och omdirigerar till inloggningssidan. -->
-                        <button class= 'logOut' >Logga ut</button>
+                    <button class= 'logOut' >Logga ut</button>
                 </form>
             </td>
         </tr>
